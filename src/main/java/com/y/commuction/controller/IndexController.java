f7aa2700eda1218ca576a36209f4060ec35bdc3a
yy <310484121@qq.com>
2021-08-24 15:54:23 +0800

    add index and bootstrap

---
+++ src/main/java/com/y/commuction/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.y.commuction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {return "index";}
+}

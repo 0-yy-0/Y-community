e882963e8d8d304c564896e7b46f27bd2004428b
yy <310484121@qq.com>
2021-08-24 15:04:14 +0800

    init repo

---
+++ src/main/java/com/y/commuction/CommuctionApplication.java
@@ -0,0 +1,13 @@
+package com.y.commuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuctionApplication.class, args);
+    }
+
+}
